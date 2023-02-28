725f6146cb7e53a67d5d3b18915abd1b1afa42c7
Waseem idrisi <waseem@greenlava.in>
2023-02-28 14:56:29 +0530

    Multi-Module-App

---
+++ app/src/main/java/dev/happysingh/multimodule/MainActivity.kt
@@ -0,0 +1,24 @@
+package dev.happysingh.multimodule
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.happysingh.happytoast.HappyToast
+import dev.happysingh.happytoast.showToast
+import dev.happysingh.string_formatter.Formatter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // from formatter module
+        val msg = Formatter.getHelloWorld()
+
+        // toast
+        HappyToast.showLongToast(this,msg)
+
+        // toast - extension
+        showToast(msg)
+    }
+}
